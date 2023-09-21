e12532b9b9c13e35c10e1d33812cb0c081927a1c
dgress95 <108832982+dgress95@users.noreply.github.com>
2023-09-21 11:58:46 -0500

    App created and routes set up

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:world_time_app/pages/choose_location.dart';
+import 'package:world_time_app/pages/home.dart';
+import 'package:world_time_app/pages/loading.dart';
+
+void main() => runApp(const WorldTimeApp());
+
+class WorldTimeApp extends StatelessWidget {
+  const WorldTimeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/home',
+      routes: {
+        '/': (context) => Loading(),
+        '/home': (context) => Home(),
+        '/location' : (context) => ChooseLocation(),
+      },
+    );
+  }
+}
